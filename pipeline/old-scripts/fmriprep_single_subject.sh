1031ba12b0f535ad41a4310fe3f6ba8391e88dd2
Nicol Lo <nlo@mit.edu>
2020-10-31 10:27:04 -0400

    update SBATCH args, delete old files and add old bash scripts for reference

---
+++ pipeline/old-scripts/fmriprep_single_subject.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# run fmriprep container for one subject
+# this script takes in args
+#   $1 - base directory
+#   $2 - array of subjects
+
+#SBATCH -J abide-fmriprep
+#SBATCH -t 16:00:00
+#SBATCH --mem=8GB
+#SBATCH --cpus-per-task=4
+
+BASE=$1
+DATASET=$2
+SITE=$3
+
+args=($@)
+subjs=(${args[@]:3}) # drop initial arg (base)
+
+# container args
+DATADIR=/BASE/${DATASET}/${SITE}
+OUTDIR=/BASE/${DATASET}/${SITE}/derivatives
+WORKDIR='/BASE/fmriprep_work_dir'
+
+SUBJECT=${subjs[${SLURM_ARRAY_TASK_ID}]}
+
+module add openmind/singularity/3.0.3
+IMGPATH="/storage/gablab001/data/singularity-images/fmriprep-v1.3.0p2.sif"
+FS_LICENSE='/home/nlo/.freesurfer_license.txt'
+
+
+echo Submitted job for: ${subject}
+
+# run container
+singularity exec -B $BASE:/BASE -e $IMGPATH \
+fmriprep $DATADIR $OUTDIR participant --participant_label $SUBJECT --nthreads 4 \
+--output-space fsaverage6 \
+--use-aroma --ignore-aroma-denoising-errors \
+--skip-bids-validation --mem_mb 7800 --fs-license-file $FS_LICENSE \
+--ignore slicetiming -w $WORKDIR --cifti-output 
+
---
+++ pipeline/old-scripts/submit_job_array.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+base=/scratch/Thu/nlo
+dataset=abide
+site=CMU_a
+
+# first go to data directory, grab all subjects,
+# and assign to an array
+pushd $base/$dataset/$site
+subjs=($(ls sub-* -d))
+popd
+
+# take the length of the array
+# this will be useful for indexing later
+len=$(expr ${#subjs[@]} - 1) # len - 1
+
+echo Spawning ${#subjs[@]} sub-jobs.
+
+sbatch --array=0-$len fmriprep_single_subject.sh $base $dataset $site ${subjs[@]}
